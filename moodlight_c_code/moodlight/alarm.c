bf6af2ede257b448c03fa60a1e5d4e790711c069
unknown <felix@baumann-juergen.ch>
2018-05-08 15:35:46 +0200

    Timer 2 config

--- moodlight_c_code/moodlight/alarm.c
+++ moodlight_c_code/moodlight/alarm.c
@@ -11,10 +11,15 @@
 #define INIT_CH1 ((uint8_t)(0x3F))
 #define INIT_CH2 ((uint8_t)(0xFF-INIT_CH1))
 
-void alarm_initPWM(void)
+uint32_t sec = 0;
+uint8_t	onoff = 0;
+
+void alarm_init(void)
 {
 	//Configure pins as output
 	DDRD |= (1<<DDRD6) | (1<<DDRD5);	//PD5 and PD6 => output
+	DDRD |= (1<<DDRD4);					//PD4: output, controls amplifier standby
+	PORTD.PIN6 = 1;							//Alarm is off
 	//Timer 0 is used to generate the alarm sound
 	//Reset timer register
 	TCCR0A = 0;
@@ -26,26 +31,63 @@ void alarm_initPWM(void)
 	
 	OCR0A = INIT_CH1;
 	OCR0B = INIT_CH2;
+	
 	//Timer 2 config
+	TCCR2A = 0;									//clear timer register
+	TCCR2B = 0;
+	ASSR = 0;
+	TIMSK2 = 0;
+			
+	TCCR2A |= (1<<WGM21);						// Clear Timer on Compare Match
+	TCCR2B |= (1<<CS22) | (1<<CS21) | (1<<CS20)	//Prescaler = 1024
+	ASSR|= (1 << AS2);							//Enable external clock
+	OCR2A = 31;									//Interrupt every 1 second
+	TIMSK2 |= (1 << OCIE2A);					//Enable interrupt for overflow
+	
+	
+	
+}
+void init_alarmTimer(void)
+{
 	
 }
 void resetAlarm(void)
 {
-	
+	stopAlarmSound();
 }
-uint16_t getAlarmTime(void)
+uint32_t getAlarmTime(void)
 {
-	return 0;
+	return sec;
 }
-void setAlarmTime(uint16_t time)
+void setAlarmTime(uint32_t time)
 {
-	
+	sec = time;
 }
 void startAlarmSound(void)
 {
+	PORTD ^= (1<<PORTD6);
 	
 }
 void stopAlarmSound(void)
 {
 	
+}
+void TIMER2_IRQ(void)
+{
+	sec--;						//decrement seconds by 1
+	if(sec == 0)				
+	{
+		init_alarmTimer();
+		if(onoff == 1)			//turn alarm sound on and off every 1 second
+		{
+			startAlarmSound();
+			onoff = 0;
+		}
+		else
+		{
+			stopAlarmSound();
+			onoff = 1;
+		}
+		
+	}
 }
