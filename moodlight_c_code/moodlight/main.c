29c43c3d970714a8cba72a21ed42459f4ce7fb71
unknown <felix@baumann-juergen.ch>
2018-05-22 11:39:01 +0200

    alarm uart

--- moodlight_c_code/moodlight/main.c
+++ moodlight_c_code/moodlight/main.c
@@ -4,8 +4,6 @@
  * Created: 06.04.2018 09:32:30
  * Author : jonas
  */ 
-
-#include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
@@ -13,38 +11,30 @@
 #include "Headerfiles/alarm.h"
 #include "Headerfiles/UART.h"
 
-#define F_CPU 8000000UL
 
 ISR(TIMER2_COMPA_vect)
 {
 	TIMER2_IRQ();
 }
-int main(void)
-{
-	
-	uint8_t counter = 0;
-	
-	alarm_init();
-	LED_initPWM();
-	UART_init();
-	setAlarmTime(30);
-	setAlarmDuration(60);
-
-    while (1) 
-    {
-		/*counter++;
-		alarm_setVol(counter);
-		if (counter >= 0x7F)
-		{
-			counter = 0;
-		}
-		_delay_ms(100);*/
-	}
-}
 ISR(USART1_RX_vect)
 {
 	RX_IRQ();
 }
+
+void main(void)
+{
+	alarm_init();
+	LED_initPWM();
+	UART_init();
+	
+	setAlarmDuration(20);		//set duration of the alarm
+
+    while (1) 
+    {
+		//wait for event (interrupts)
+	}
+}
+
 
 
 
